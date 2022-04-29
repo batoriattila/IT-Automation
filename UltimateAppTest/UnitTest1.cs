59dd75fe5ca0dc39f9cb2236163e0c121e0ad703
IT Üzemeltetés <itlab@aut.bme.hu>
2022-04-29 11:34:06 +0200

    teszt kiegészítve

---
+++ UltimateAppTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace UltimateApp.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            int? retVal = Calculator.calculate(2, 3, Operation.Add);
+            Assert.AreEqual(5, retVal);
+        }
+    }
+}
